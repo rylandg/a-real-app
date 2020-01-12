4cd899290d66fa18ed36c9c85ac3495794d82a74
Ryland Goldstein <ryland@binaris.com>
2020-01-12 02:54:58 +0000

    remove react-showcase specific files

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,42 +1,7 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
-
 import './styles/HomePage.scss';
 
 export const HomePage: React.FC = () => {
-  return (
-    <ul>
-      <li>
-        <Link to='/simple-function-component'>
-          Simple function component
-        </Link>
-      </li>
-      <li>
-        <Link to='/simple-class-component'>
-          Simple class component
-        </Link>
-      </li>
-      <li>
-        <Link to='/function-component-with-state'>
-          Function component with state
-        </Link>
-      </li>
-      <li>
-        <Link to='/class-component-with-state'>
-          Class component with state
-        </Link>
-      </li>
-      <li>
-        <Link to='/styled-component'>
-          Styled component
-        </Link>
-      </li>
-      <li>
-        <Link to='/drag-and-drop'>
-          Drag and drop example
-        </Link>
-      </li>
-    </ul>
-  );
+  return null;
 }
--- src/Router.tsx
+++ src/Router.tsx
@@ -4,41 +4,10 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { HomePage } from './HomePage';
-import { SimpleFunctionComponent } from './SimpleFunctionComponent/SimpleFunctionComponent';
-import { SimpleClassComponent } from './SimpleClassComponent/SimpleClassComponent';
-import { StyledComponent } from './StyledComponent/StyledComponent';
-import { DragAndDropComponent } from './DragAndDropComponent/DragAndDropComponent';
-import { ClassComponentWithState } from './ClassComponentWithState/ClassComponentWithState';
-import { FunctionComponentWithState } from './FunctionComponentWithState/FunctionComponentWithState';
-
 
 const Router: React.FC = () => {
   return (
     <Switch>
-      <Route
-        component={SimpleFunctionComponent}
-        path='/simple-function-component'
-      />
-      <Route
-        component={SimpleClassComponent}
-        path='/simple-class-component'
-      />
-      <Route
-        component={FunctionComponentWithState}
-        path='/function-component-with-state'
-      />
-      <Route
-        component={ClassComponentWithState}
-        path='/class-component-with-state'
-      />
-      <Route
-        component={StyledComponent}
-        path='/styled-component'
-      />
-      <Route
-        component={DragAndDropComponent}
-        path='/drag-and-drop'
-      />
       <Route component={HomePage} />
     </Switch>
   );
