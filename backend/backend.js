2cd7d4520d5fa1dd152759672826f840cbaa6b39
Ryland Goldstein <ryland@binaris.com>
2020-01-12 02:57:22 +0000

    remove react-showcase backend files

--- backend/backend.js
+++ backend/backend.js
@@ -1,11 +1,4 @@
 import { update, get } from '@reshuffle/db';
 
 /* @expose */
-export async function getRemoteOrnaments() {
-  return await get('temp');
-}
-
-/* @expose */
-export async function saveRemoteOrnaments(ornaments) {
-  return await update('temp', (oldOrnaments) => ornaments);
-}
+export async function placeholder() {}
